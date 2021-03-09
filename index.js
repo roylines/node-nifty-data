b413d802711a679ca4f7582c9ae30599bbb73d8e
roylines <roy@roylines.co.uk>
2021-03-09 07:06:49 +0000

    refactored history

--- index.js
+++ index.js
@@ -1,41 +1,4 @@
-const axios = require("axios");
-const api = axios.create({
-  baseURL: "https://api.niftygateway.com/market",
-});
-
-const globalHistory = async ({ contract, size = 10, onlySales = true }) => {
-  const body = {
-    contractAddress: contract,
-    niftyType: 1,
-    current: 1,
-    size,
-    onlySales: onlySales.toString(),
-    cancelToken: { promise: {} },
-    timeout: 30000,
-  };
-
-  const ret = await api.post("nifty-history-by-type/", body);
-  return ret.data.data.results;
-};
-
-const editionHistory = async ({ contract, token, size = 10 }) => {
-  const body = {
-    contractAddress: contract,
-    tokenId: token,
-    current: 1,
-    size,
-  };
-  const ret = await api.post("nifty-secondary-market/", body);
-  return ret.data.data.results;
-};
-
-const history = async ({ token, ...rest }) => {
-  if (token) {
-    return await editionHistory({ token, ...rest });
-  } else {
-    return await globalHistory(rest);
-  }
-};
+const { history } = require("./lib/history");
 
 module.exports = {
   history,
--- index.test.js
+++ index.test.js
@@ -2,20 +2,38 @@ const { history } = require("./index");
 
 describe("nifty-data", () => {
   describe("history", () => {
-    it("can get global", async () => {
+    it("can get global (all)", async () => {
       const q = {
-        contract: "0xe9be55ffedb6c2a2f3f8eac31c60d7f122f79958",
+        nifty: {
+          contract: "0xe9be55ffedb6c2a2f3f8eac31c60d7f122f79958",
+        },
       };
 
       const ret = await history(q);
+      expect(ret.length).toBeGreaterThan(0);
+    });
+    it("can get global (sales only)", async () => {
+      const q = {
+        nifty: {
+          contract: "0xe9be55ffedb6c2a2f3f8eac31c60d7f122f79958",
+        },
+        salesOnly: true,
+      };
+
+      const ret = await history(q);
+      expect(ret.length).toBeGreaterThan(0);
     });
     it("can get local", async () => {
       const q = {
-        contract: "0xe9be55ffedb6c2a2f3f8eac31c60d7f122f79958",
-        token: "11400012315",
+        nifty: {
+          contract: "0xe9be55ffedb6c2a2f3f8eac31c60d7f122f79958",
+          token: "11400012315",
+        },
+        size: 1,
       };
 
       const ret = await history(q);
+      expect(ret.length).toBeGreaterThan(0);
     });
   });
 });
